5a531227f7e08a60cf551ab7d52471d07a1311c8
Lamson Le <ll217@u.washington.edu>
2015-05-04 09:29:48 -0700

    add upload file

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,20 @@
 var querystring = require('querystring');
+var fs = require('fs');
+var formidable = require('formidable');
 
-function start(res, postData) {
+function start(res) {
   console.log("Request handler 'start' was called.");
 
   var body = '<html>' +
     '<head>' +
-    '<meta http-equiv="Content-Type" content="text/html; ' +
-    'charset=UTF-8" />' +
+    '<meta http-equiv="Content-Type" ' +
+    'content="text/html; charset=UTF-8" />' +
     '</head>' +
     '<body>' +
-    '<form action="/upload" method="post">' +
-    '<textarea name="text" rows="20" cols="60"></textarea>' +
-    '<input type="submit" value="Submit text" />' +
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">' +
+    '<input type="file" name="upload" multiple="multiple">' +
+    '<input type="submit" value="Upload file" />' +
     '</form>' +
     '</body>' +
     '</html>';
@@ -21,13 +24,33 @@ function start(res, postData) {
     res.end();
 }
 
-function upload(res, postData) {
+function upload(res, req) {
   console.log('Request handler "upload" was called.');
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("You've sent the text: " +
-    querystring.parse(postData).text);
-  res.end();
+
+  var form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(req, function(err, fields, files) {
+    console.log('parsing done');
+
+    fs.rename(files.upload.path, './test.png', function(err) {
+      if (err) {
+        fs.unlink('./test.png');
+        fs.rename(files.upload.path, './test.png');
+      }
+    });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("received image:<br/>");
+    res.write("<img src='/show' />");
+    res.end();
+  });
+}
+
+function show(res) {
+  console.log("Request handler 'show' was called.");
+  res.writeHead(200, {'Content-Type': "image/png"});
+  fs.createReadStream('./test.png').pipe(res);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
